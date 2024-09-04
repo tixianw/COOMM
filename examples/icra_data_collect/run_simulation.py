5b23f1b51acdd75c43ecc4ce61486a582aba826c
tixianw <tixianw@gmail.com>
2024-09-04 01:09:59 -0500

    finished running 64 simulations for the data set

--- examples/icra_data_collect/run_simulation.py
+++ examples/icra_data_collect/run_simulation.py
@@ -3,8 +3,8 @@ Created on Sep. 23, 2021
 @author: Heng-Sheng (Hanson) Chang
 """
 
-from collections import defaultdict
 import numpy as np
+from collections import defaultdict
 from tqdm import tqdm
 
 import sys
